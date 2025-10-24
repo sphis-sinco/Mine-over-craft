6365df805645df085a6d19e45dc70640d0685917
Viwu <sinconsistencia@gmail.com>
2025-10-23 23:58:57 -0500

    1.0 changelog iter 1

---
+++ changelogs/1.0/MineOverCraft-1.0-2025-9-23.md
@@ -0,0 +1,3 @@
+# MineOverCraft v1.0
+- [ADDITION] Harsan Block
+- [TWEAK] On a new world you are sent to the MOCD (Dummy Name) Dimension
---
+++ changelogs/1.0/runner.bat
@@ -0,0 +1,3 @@
+cd ../../
+cd .dev/scripts/changelog-generator
+haxe runner.hxml -D file_input_path="..\..\..\changelogs/1.0/changelog-1.0" -D file_output_path_prefix="..\..\..\changelogs/1.0/"
