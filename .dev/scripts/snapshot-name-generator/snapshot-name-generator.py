f0da458f980020c0430e87a32c8d465f529f5707
Viwu <sinconsistencia@gmail.com>
2025-10-25 16:41:16 -0500

    snapshots

--- .dev/scripts/snapshot-name-generator/snapshot-name-generator.py
+++ .dev/scripts/snapshot-name-generator/snapshot-name-generator.py
@@ -1,10 +1,10 @@
-import datetime, sys, os
+import datetime
 
 current_year = datetime.date.today().year
 current_year_week = datetime.date.today().isocalendar()[1]
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def get_snapshot():
-    return str(current_year) + 'w' + str(current_year_week)+alphabet[int(input("How many updates have there been this week?\n> "))]
+    return str(current_year) + 'w' + str(current_year_week)+alphabet[int(input("How many snapshots have there been this week?\n> "))]
 
 print("Snapshot "+ str(get_snapshot()))
